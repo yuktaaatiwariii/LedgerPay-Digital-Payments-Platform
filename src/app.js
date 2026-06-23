1ee4fee3eb0deeaf5142e49420e7ba3dd2f436b6
yuktaaatiwariii <yuktatiwari0@gmail.com>
2026-06-23 20:57:11 +0530

    backend cors setup

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
+
 
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
 app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:5173', // Adjust the origin as needed
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+}));
 
 const authRouter = require ('./routes/auth.routes');
 const accountRouter = require('./routes/account.routes');
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const {userRegisterController, userLoginController,userLogoutController} = require('../controllers/auth.controller');
+const authMiddleware = require('../middleware/auth.middleware');
+
 
 router.post('/register', userRegisterController );
 router.post('/login', userLoginController );
 router.post('/logout', userLogoutController );
+
+router.get("/me",authMiddleware.authMiddleware, (req, res) => res.status(200).json({success:true,user:req.user}));
 
 module.exports = router;
